1d3e053a03bad2d20d5506154058eff10ddaaf22
Tan Li Hau <lhtan93@gmail.com>
2018-11-08 02:23:04 +0800

    ! update typewriting

--- sw.js
+++ sw.js
@@ -15,5 +15,5 @@ workbox.precaching.precacheAndRoute([
   { url: '/main.min.js', revision: 'bd9a94f' },
   { url: '/index.min.css', revision: '9ac1bcd' },
   { url: '/assets/tan_li_hau.png', revision: '09c88d4' },
-  { url: '/index.html', revision: '12ef00f' }
+  { url: '/index.html', revision: '5f38175' }
 ]);
