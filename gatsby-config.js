75e259bc699e14a02b5e30e71faee449c60a3637
Tan Li Hau <lhtan93@gmail.com>
2019-03-10 21:34:29 +0800

    add: add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-135921142-1`,
       },
     },
     `gatsby-plugin-feed`,
