f8ac12a54e3379d17e774004908d148d7fd410e5
Tan Li Hau <lhtan93@gmail.com>
2019-03-10 23:39:12 +0800

    chore: update site title

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ class BlogIndex extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
-          title="All posts"
+          title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
         <Bio />
