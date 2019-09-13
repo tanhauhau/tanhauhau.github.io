61e920cf486db344576386eab47f8b29e3dec460
Tan Li Hau <lhtan93@gmail.com>
2019-09-13 23:11:06 +0800

    Built on Fri Sep 13 2019 23:11:06 GMT+0800 (Singapore Standard Time)

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1335c2b6b1e5fa0aa4b2.js"
+    "url": "webpack-runtime-6d5256b6a19fe8021399.js"
   },
   {
     "url": "styles.a8c4ad346a5806bac5ff.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2baf448f2421ef56d00345d9fe033a32"
+    "revision": "e4beb2e5a9867cb71c43a4fb5b9ae118"
   },
   {
     "url": "component---src-pages-404-js-cd68818fa1fba6728cb6.js"
@@ -52,11 +52,11 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/404.html/page-data.json",
-    "revision": "c8ad255b93b3c9460600cfa180af4dc3"
+    "revision": "9eca9baf88af60d1448f4b30d1db8fc2"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "f68fbba6f21172dd925a70c672d923fb"
+    "revision": "448ceb1dfc5b6ef5f36c882c570e66d7"
   },
   {
     "url": "manifest.webmanifest",
