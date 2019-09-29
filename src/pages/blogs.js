0447d98e3c50bcbe1ea4318e720abab5f4cd1f74
Tan Li Hau <lhtan93@gmail.com>
2019-09-29 12:33:58 +0800

    feat: add rss link to bloglist

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -16,7 +16,33 @@ function BlogIndex({ data, location }) {
         title="Li Hau's Blog"
         keywords={[`blog`, `gatsby`, `javascript`, `react`]}
       />
-      <h1>Li Hau's Blog</h1>
+      <h1>
+        Li Hau's Blog{' '}
+        <a href="/blog-rss.xml" target="_blank">
+          <svg
+            viewBox="0 0 30 30"
+            style={{
+              width: 15,
+              height: 15,
+              verticalAlign: 'bottom',
+              position: 'relative',
+              bottom: 4,
+              fill: 'none',
+              stroke: '#612e77',
+            }}
+          >
+            <path
+              d="M6.503 20.752c0 1.794-1.456 3.248-3.251 3.248-1.796
+            0-3.252-1.454-3.252-3.248 0-1.794 1.456-3.248 3.252-3.248 1.795.001
+            3.251 1.454 3.251 3.248zm-6.503-12.572v4.811c6.05.062 10.96 4.966
+            11.022
+            11.009h4.817c-.062-8.71-7.118-15.758-15.839-15.82zm0-3.368c10.58.046
+            19.152 8.594 19.183
+            19.188h4.817c-.03-13.231-10.755-23.954-24-24v4.812z"
+            />
+          </svg>
+        </a>
+      </h1>
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug;
         return (
