22ea567a67e535dccadddac8de7ff786466e904b
Tan Li Hau <lhtan93@gmail.com>
2019-12-15 03:09:08 +0800

    Built on Sun Dec 15 2019 03:09:08 GMT+0800 (Singapore Standard Time)

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-94be106bb5238cfaa91d.js"
+    "url": "webpack-runtime-c0c14824a2c2f58dd811.js"
   },
   {
     "url": "styles.66ca4bc5172c2218813e.css"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a4299ddcde3f403290c526df1d7597b7"
+    "revision": "34e75b2c1625c21f52d64bcafa12246e"
   },
   {
     "url": "manifest.webmanifest",
