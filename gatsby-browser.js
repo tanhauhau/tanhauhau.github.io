88f8780fdb47db87d2fb22348cb39d54a478c182
Tan Li Hau <lhtan93@gmail.com>
2020-03-04 08:44:16 +0800

    feat: remove dark mode switch

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,14 +2,3 @@
 import 'typeface-varela-round';
 import 'typeface-lora';
 import './src/styles/global.css';
-// import React from 'react';
-// import DarkModeSwitch from './src/components/components/DarkModeSwitch';
-
-// export const wrapRootElement = ({ element }) => {
-//   return (
-//     <>
-//       {element}
-//       <DarkModeSwitch />
-//     </>
-//   );
-// };
