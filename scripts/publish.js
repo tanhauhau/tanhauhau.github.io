83b2e30226491c6fadd5ca245c4516d9aef1511c
tanhauhau <lhtan93@gmail.com>
2024-03-31 00:32:44 +0800

    feat: restore the release script

--- scripts/publish.js
+++ scripts/publish.js
@@ -19,26 +19,26 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 	}
 
 	// clean cache folder
-	// exec('git clean -df');
+	exec('git clean -df');
 
-	// exec('git checkout master');
+	exec('git checkout master');
 
-	// // make sure branch is always up to date
-	// exec('git fetch origin master');
-	// exec('git reset --hard origin/master');
+	// make sure branch is always up to date
+	exec('git fetch origin master');
+	exec('git reset --hard origin/master');
 
-	// exec('git rm -rf .');
+	exec('git rm -rf .');
 
-	// const files = await fs.readdir(outputFolder);
-	// for (const file of files) {
-	// 	await fs.copy(path.join(outputFolder, file), path.join(cacheFolder, file));
-	// }
+	const files = await fs.readdir(outputFolder);
+	for (const file of files) {
+		await fs.copy(path.join(outputFolder, file), path.join(cacheFolder, file));
+	}
 
-	// exec('git add .');
-	// exec(`git config user.email "lhtan93@gmail.com"`);
-	// exec(`git config user.name "Github Actions"`);
-	// exec(`git commit -m "Built on ${String(new Date())}"`);
-	// exec('git push origin master');
+	exec('git add .');
+	exec(`git config user.email "lhtan93@gmail.com"`);
+	exec(`git config user.name "Github Actions"`);
+	exec(`git commit -m "Built on ${String(new Date())}"`);
+	exec('git push origin master');
 
 	function exec(cmd) {
 		console.log(chalk.dim(cmd));
